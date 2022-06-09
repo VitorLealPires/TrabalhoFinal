a4eb65cb27f49cab9605f2cfa5d22c6f9917e59a
BrunoAAM <brunoandradeaguiarmarques@hotmail.com>
2022-06-08 23:43:49 -0300

    Implementando Subtração e Multiplicação

--- Calculadora.java
+++ Calculadora.java
@@ -1,7 +1,18 @@
 public class Calculadora{
+
     public double soma(double numeroA, double numeroB) {
 
         return numeroA + numeroB;
+    }
+
+    public double subtracao(double numeroA, double numeroB) {
+
+        return numeroA - numeroB;
+    }
+
+    public double multiplicacao(double numeroA, double numeroB) {
+
+        return numeroA * numeroB;
     }
 
 }
